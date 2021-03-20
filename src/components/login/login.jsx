d0089765dc3ce30a6fbd330789b461e08bc1ffd9
Merry <wiostz98kr@gmail.com>
2021-03-20 15:08:28 +0900

    add login and logout functionality

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styles from './login.module.css';
 
 const Login = ({ authService }) => {
+    const history = useHistory();
+    const goToMaker = userId => {
+        history.push({
+            pathname: '/maker',
+            state: { id: userId },
+        });
+    }
     const onLogin = event => {
         authService
             .login(event.currentTarget.textContent)
-            .then(console.log);
+            .then(data => goToMaker(data.user.uid));
     };
+    useEffect(() => {
+        authService.onAuthChange(user => {
+            //사용자가 로그인을 했다면 user라는 데이터가 있고, 안했다면 user는 null임
+            user && goToMaker(user.uid)
+        })
+    });
     // const handleLogoout = event => {
 
     // }
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -6,8 +6,13 @@ class AuthService {
         const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
         return firebaseApp.auth().signInWithPopup(authProvider);
     }
-    // logout(){
-    //     firebase.auth().signOut().then(console.log);
-    // }
+    logout() {
+        firebase.auth().signOut();
+    }
+    onAuthChange(onUserChanged) {
+        firebase.auth().onAuthStateChanged(user => {
+            onUserChanged(user);
+        })
+    }
 }
 export default AuthService;
