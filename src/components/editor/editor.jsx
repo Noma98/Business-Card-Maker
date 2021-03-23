90e423ce9513949a6552512a193a87aec6d86091
Merry <wiostz98kr@gmail.com>
2021-03-23 14:57:41 +0900

    add CardAddForm component

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import CardAddForm from '../card_add_form/card_add_form';
 import CardEditForm from '../card_edit_form/card_edit_form';
 import styles from './editor.module.css';
 
-const Editor = ({ cards }) => <section className={styles.editor}>
+const Editor = ({ cards, addCard }) => <section className={styles.editor}>
     <h1 className={styles.title}>Card Maker</h1>
     {
-        cards.map(card => <CardEditForm card={card} />)
+        cards.map(card => <CardEditForm card={card} key={card.id} />)
     }
+    <CardAddForm onAdd={addCard} />
 </section>
 
 
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -53,11 +53,15 @@ const Maker = ({ authService }) => {
             }
         });
     });
+    const addCard = card => {
+        const updated = [...cards, card];
+        setCards(updated);
+    };
     return (
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
             <div className={styles.container}>
-                <Editor cards={cards} />
+                <Editor cards={cards} addCard={addCard} />
                 <Preview cards={cards} />
             </div>
             <Footer />
